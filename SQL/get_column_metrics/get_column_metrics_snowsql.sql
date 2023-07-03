bcfbac0263e617912e5add408293aa3acf90eb24
Jon T <jtouvelle@mathematica-mpr.com>
2023-07-02 20:55:49 -0400

    update

--- SQL/get_column_metrics/get_column_metrics_snowsql.sql
+++ SQL/get_column_metrics/get_column_metrics_snowsql.sql
@@ -1,13 +1,27 @@
 -- get_column_metrics
 EXECUTE IMMEDIATE $$
 DECLARE
+  schema_name := 'OFFICEALLY';
+  table_name := 'CLM_TEST';
   c1 CURSOR FOR select column_name from t1;
 BEGIN
-    create or replace temp table test(exmaple_value varchar);
+    create or replace temp table column_metrics(column_name varchar, example_value varchar, updated_at timestamp);
   FOR rec IN c1 DO
-     LET stmt := 'INSERT INTO test SELECT top 1  '|| rec.column_name ||' FROM clm_test';
-     EXECUTE IMMEDIATE stmt;
+     LET cname varchar := rec.column_name;
+     LET stmt := 
+     'INSERT INTO column_metrics 
+     SELECT top 1 
+     '''|| cname ||'''              as column_name,  
+     '|| rec.column_name ||'        as exmaple_value,
+     CURRENT_TIMESTAMP              as timestamp
+     FROM '||schema_name||'.'||table_name;
+     EXECUTE IMMEDIATE stmt;     
   END FOR;
-  
+    
 END;
 $$
+;
+//
+//SELECT * FROM clm_test
+SELECT * FROM column_metrics;
+SELECT  * FROM TEST;
